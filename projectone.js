5d60f763c844aaa27cd6ed0120122f00726d5164
kisublee <yigi2021@gmail.com>
2021-07-14 21:14:41 -0400

    Add getUserChoice function.

--- projectone.js
+++ projectone.js
@@ -3,3 +3,13 @@ ROCK, PAPER, OR SCISSORS PROJECT
 --------------------------------
 Write your functions below.
 */
+
+
+function getUserChoice (userInput) {
+    userInput.toLowerCase()
+
+    if (!userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
+        console.log("Error!")
+    }
+    return (userInput)
+}
